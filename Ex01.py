c7870cd476c62d2053d8c4d8a2ad1501f80092b5
eunseop1 <102935072+eunseop1@users.noreply.github.com>
2022-08-17 08:40:33 +0900

    Add files via upload

---
+++ Ex01.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+url = "http://www.naver.com/"
+
+# 1. 옵션지정
+options = Options()
+# 2. 크롬드라이버를 로드하여 브라우저 열기
+driver = webdriver.Chrome(executable_path="./chromedriver.exe", options=options)
+
+# 3. 네이버를 접속해보자
+driver.get(url)
+
+
+
+# 4. 부라우져 닫기
+driver.quit()
